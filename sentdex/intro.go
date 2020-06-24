ece0217be4259b1a7f3f27bcee2e81627196c558
Julia Guo <jzg2110@columbia.edu>
2020-06-24 18:15:54 -0400

    funcs and types, ptrs, webserver

---
+++ sentdex/intro.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func add(x, y float64) float64 {
+	return x + y
+}
+
+func multiple(a, b string) (string, string) {
+	return b, a
+}
+
+func main() {
+	num1, num2 := 5.6, 9.5
+	fmt.Println(add(num1, num2))
+
+	w1, w2 := "hey", "there"
+
+	fmt.Println(multiple(w1, w2))
+
+	// var a int = 62
+	// var b float64 = float64(a)
+
+	// x := a // x will be type int
+}
